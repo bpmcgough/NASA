819ab470ac7364614ac9005443f46480e43c55b5
davidnoah <david.t.noah@gmail.com>
2016-12-15 19:00:22 -0700

    implemented static assets

--- controllers/pictureOfTheDay.js
+++ controllers/pictureOfTheDay.js
@@ -1,15 +1,17 @@
 const RapidAPI = new require('rapidapi-connect');
 const rapid = new RapidAPI('SpaceToday', 'a7c7a9c7-dd7a-4bb0-a855-8dede92148ee');
 
-exports.findPicture = function(req, res){
+exports.findPicture = function(req, res) {
   rapid.call('NasaAPI', 'getPictureOfTheDay', {
 	'apiKey': process.env.NASA_API,
 	'date': '',
 	'highResolution': ''
 
   }).on('success', (payload)=>{
+    console.log("success");
     res.send(payload);
   }).on('error', (payload)=>{
+    console.log("what?");
     res.send("failure");
   });
 };
